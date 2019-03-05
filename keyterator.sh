92dd4de6feb47f6a72046dab2a498d07ec16065f
Victor Alvarez <valvarez430@gmail.com>
2019-03-05 01:34:06 +0000

    First commit

---
+++ keyterator.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# KEYTERATOR
+# Iterate over your passwords and keys and improve your memory
+
+# Recall user to send end process when finished
+echo "Press [CTRL+C] to stop."
+
+# Infinite LOOP
+# read and hide entered string and then redirect to /dev/null
+while true
+do read -s > /dev/null
+done
+
+# Another way to iterate
+# while : # Null command, exit status is always true
